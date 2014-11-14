08ed6c880411c4ed2a6bbd1ec757d5aa93c5c53d
Luke Bergen <lukeabergen@gmail.com>
2014-11-14 18:06:20 -0500

    Adding camera controller and testing slope walking

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = player.transform.position;
+		transform.position = new Vector3 (pos.x, transform.position.y, transform.position.z);
+	}
+}
