9a82e5b23bb3ecdca44f283831357c4b0f0fa9b6
Mark <mark.brezinski@gmail.com>
2014-11-21 16:13:50 -0800

    Climbing Ones!
    • Made animations
    • Imported assets
    • Created a simple script with some notes on functionality, since
    animating this is going to be slightly tricky.

---
+++ Assets/Scripts/ClimbingOneMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+private Animator animator;
+public bool updatePosition;
+public bool playMovementAnim;
+public bool move;
+public bool readyToMove = false;
+
+public class ClimbingOneMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		/* • If 'move' (set from an AI script)
+		 * • Play the movement animation
+		 * • When the animation is complete, update transform.position to "teleport" the transform up 0.63 units
+		 * • Update "readyToMove" so if the AI calls for future movement, it can occur.
+		 * */
+
+		AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+		if(move && stateInfo.nameHash == Animator.StringToHash("ClimbingOneClimbing")) {
+			readyToMove = false;
+			}
+		if(move && stateInfo.nameHash == !Animator.StringToHash("ClimbingOneClimbing")) {
+			readyToMove = true;
+		}
+}
