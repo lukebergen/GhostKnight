35b9f8d9e527aef325a19026502e3bd3eaea2310
Luke Bergen <lukeabergen@gmail.com>
2014-12-06 13:36:27 -0500

    Introducing crouchAttack and sending (for now) 'OnPet'.

--- Assets/Scripts/BalastController.cs
+++ Assets/Scripts/BalastController.cs
@@ -3,20 +3,21 @@ using System.Collections;
 
 public class BalastController : MonoBehaviour {
 
-	private GameObject player;
 	private bool pickupable = false;
 
 	public void Start() {
-		player = GameObject.Find ("Player");
 	}
 
-	public void CutInteraction() {
-		if (pickupable) {
-			Debug.Log ("Picking up balast thing");
-			player.GetComponent<ProtagAttackController>().pickUp(gameObject);
-		} else {
+	public void OnCut(GameObject cutter) {
+		if (!pickupable) {
 			rigidbody2D.isKinematic = false;
 			pickupable = true;
 		}
 	}
+
+	public void OnPet(GameObject petter) {
+		Debug.Log ("Petting");
+	}
+
+	// player.GetComponent<ProtagAttackController>().pickUp(gameObject);
 }
--- Assets/Scripts/ProtagAttackController.cs
+++ Assets/Scripts/ProtagAttackController.cs
@@ -8,6 +8,7 @@ public class ProtagAttackController : MonoBehaviour {
 
 	private bool doAttack = false;
 	private bool doAttack2 = false;
+	private bool doCrouchAttack = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +16,11 @@ public class ProtagAttackController : MonoBehaviour {
 
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
-			doAttack = true;
+			if (Input.GetAxis("Vertical") < 0) {
+				doCrouchAttack = true;
+			} else {
+				doAttack = true;
+			}
 		}
 		if (Input.GetButtonDown ("Fire2")) {
 			doAttack2 = true;
@@ -24,17 +29,23 @@ public class ProtagAttackController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (doAttack) {
-			doAttack = false;
-			int direction = gameObject.GetComponent<ProtagController>().direction;
-			Vector2 p1 = new Vector2(transform.position.x + (direction * 0.15f), transform.position.y + 0.1f);
-			Vector2 p2 = new Vector2(transform.position.x + (direction * 0.15f), transform.position.y - 0.1f);
-			Debug.DrawLine(new Vector3(p1.x, p1.y, -0.5f), new Vector3(p2.x, p2.y, -0.5f), Color.blue, 10.0f);
-			Collider2D[] collisions = Physics2D.OverlapAreaAll(p1, p2);
+		if (doAttack || doCrouchAttack) {
+			int direction = gameObject.GetComponent<ProtagController> ().direction;
+			Vector2 p1 = new Vector2 (transform.position.x + (direction * 0.15f), transform.position.y + 0.1f);
+			Vector2 p2 = new Vector2 (transform.position.x + (direction * 0.15f), transform.position.y - 0.1f);
+			Collider2D[] collisions = Physics2D.OverlapAreaAll (p1, p2);
+
 			for (int i = 0 ; i < collisions.Length ; i++) {
 				InteractionsController other = collisions[i].gameObject.GetComponent<InteractionsController>();
-				if (other != null && other.hasType(InteractionsController.InteractionType.Cuttable)) {
-					other.gameObject.SendMessage("CutInteraction");
+				if (other != null) {
+					if (doAttack) {
+						doAttack = false;
+						other.gameObject.SendMessage("OnCut", gameObject);
+					}
+					if (doCrouchAttack) {
+						doCrouchAttack = false;
+						other.gameObject.SendMessage("OnPet", gameObject);
+					}
 				}
 			}
 		}
