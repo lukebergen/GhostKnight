f4f82f6f213531d0ebd39fd61dc9e01be2b79da0
Luke Bergen <lukeabergen@gmail.com>
2014-12-13 18:14:56 -0500

    Fixing bug with attack controller

--- Assets/Scripts/ProtagAttackController.cs
+++ Assets/Scripts/ProtagAttackController.cs
@@ -31,20 +31,24 @@ public class ProtagAttackController : MonoBehaviour {
 	void FixedUpdate () {
 		if (doAttack || doCrouchAttack) {
 			int direction = gameObject.GetComponent<ProtagController> ().direction;
-			Vector2 p1 = new Vector2 (transform.position.x + (direction * 0.15f), transform.position.y + 0.1f);
-			Vector2 p2 = new Vector2 (transform.position.x + (direction * 0.15f), transform.position.y - 0.1f);
+			Vector2 p1 = new Vector2 (transform.position.x + (direction * 0.2f) + 0.05f, transform.position.y + 0.1f);
+			Vector2 p2 = new Vector2 (transform.position.x + (direction * 0.2f) - 0.05f, transform.position.y - 0.1f);
+
+			Debug.DrawLine(new Vector3(p1.x, p1.y, 1.0f), new Vector3(p2.x, p2.y, 1.0f), Color.red, 1.0f);
+
 			Collider2D[] collisions = Physics2D.OverlapAreaAll (p1, p2);
 
 			for (int i = 0 ; i < collisions.Length ; i++) {
 				if (doAttack) {
-					doAttack = false;
+					Debug.Log ("cutting: " + collisions[i].name);
 					collisions[i].gameObject.SendMessage("OnCut", gameObject, SendMessageOptions.DontRequireReceiver);
 				}
 				if (doCrouchAttack) {
-					doCrouchAttack = false;
 					collisions[i].gameObject.SendMessage("OnCrouchAttack", gameObject, SendMessageOptions.DontRequireReceiver);
 				}
 			}
+			doAttack = false;
+			doCrouchAttack = false;
 		}
 
 		if (doAttack2) {
